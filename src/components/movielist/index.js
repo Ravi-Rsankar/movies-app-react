ba631dccec6f2ef6475e86e70f4b1940299f559d
Ravi-Rsankar <ravisankar.r93@gmail.com>
2021-08-30 17:17:30 +0530

    first commit

---
+++ src/components/movielist/index.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import Footer from "../footer";
+import Header from "../header";
+import Search from "../search/index";
+class MovieList extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      movies: [],
+      search: "",
+      userDetail: "",
+      isLoggedIn: false,
+    };
+    this.updateSearch = this.updateSearch.bind(this);
+    this.searchAction = this.searchAction.bind(this);
+    this.isLogout = this.isLogout.bind(this);
+  }
+  updateSearch(newVal) {
+    this.setState({
+      search: newVal.target.value,
+    });
+  }
+  getMovie() {
+    fetch(
+      `https://www.omdbapi.com/?apikey=fa9a7315&s=${this.state.search}&type=movie`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          movies: data.Search,
+        });
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  searchAction() {
+    return this.state.movies
+      ? this.getMovie()
+      : alert("Please enter movie title");
+  }
+  componentWillMount() {
+    let getUserDetail = localStorage.getItem("userDetail");
+    if (getUserDetail) {
+      this.setState({
+        userDetail: JSON.parse(getUserDetail),
+        isLoggedIn: true,
+      });
+    }
+  }
+  isLogout() {
+    if (window.confirm("Are you sure?")) {
+      // alert('asdfasd');
+      localStorage.removeItem("userDetail");
+      this.setState({
+        isLoggedIn: false,
+      });
+      this.forceUpdate();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoggedIn ? (
+          <section className="fullscreen_bg">
+            <Header
+              user={this.state.userDetail}
+              loginStatus={this.state.isLoggedIn}
+              isLogout={this.isLogout}
+            />
+
+            <section className="container">
+              <Search
+                search={this.state.search}
+                detectChange={this.updateSearch}
+                searchAction={this.searchAction}
+              />
+              <div className="btn-event">
+                <button className="btn btn-success"> Add Event </button>
+              </div>
+              <div className="row movieList display-flex">
+                {this.state.movies.map((res, key) => {
+                  return (
+                    <div className="col-xs-6 col-md-4 movieBlock" key={key}>
+                      <div className="panel panel-success">
+                        <div className="panel-heading">
+                          <h3 className="panel-title">Name:{res.Title}</h3>
+                        </div>
+                        <div className="panel-body">
+                          <img
+                            alt={res.Title}
+                            src={res.Poster}
+                            className="img-responsive"
+                          />
+                        </div>
+                        <div className="panel-footer text-center">
+                          <Link to={"/movie/" + res.imdbID}>
+                            <button className="btn btn-success btn-block">
+                              {" "}
+                              View Detail{" "}
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </section>
+            <Footer />
+          </section>
+        ) : (
+          <Redirect to="/login" />
+        )}
+      </div>
+    );
+  }
+}
+export default MovieList;
